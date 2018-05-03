ebf7e2db1a9f8c598d7e0fa3529afc6e4041257f
Samuel ROCHETTE <srochette@octo.com>
2018-05-03 10:27:18 +0200

    second commiut

--- kata_api_entreprise/view.py
+++ kata_api_entreprise/view.py
@@ -1,11 +1,29 @@
+import requests
 from flask import Flask, Blueprint
 
 bp = Blueprint('myapp', __name__)
 
+API_ENTREPRISE_BASE_URL = 'https://entreprise.api.gouv.fr/v2/etablissements/'
+API_ENTREPRISE_TOKEN = 'p9A9funz6VJZGy1ZWI3d6aBIMJqIpcSB'
+API_ENTREPRISE_CONTEXT = 'Demande d\'embauche d\'un salarié étranger'
+API_ENTREPRISE_OBJECT = 'Gestion Automatisée de l\'immigration professionnelle'
+SIREN_DGEF = '130017593'
 
-@bp.route('/')
-def index():
-    return 'Hello, World!'
+
+@bp.route('/entreprise/<siret>')
+def index(siret):
+    url = '{base_url}{siret}'.format(
+        base_url=API_ENTREPRISE_BASE_URL,
+        siret=siret
+    )
+    params = {
+        'context': API_ENTREPRISE_CONTEXT,
+        'recipient': SIREN_DGEF,
+        'object': API_ENTREPRISE_OBJECT,
+        'token': API_ENTREPRISE_TOKEN
+    }
+    response = requests.get(url, params=params)
+    return response.text
 
 
 def create_app(config='dev'):
